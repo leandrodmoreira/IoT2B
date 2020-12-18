9fc3494ad590e528324005cf0b77c5c5a1123b60
leandrodmoreira <leandrodmoreira@gmail.com>
2020-12-18 08:52:55 -0300

    Airdata -> Google Sheets

--- MS430/Raspberry_Pi/gspreads_package/gspreads_functions.py
+++ MS430/Raspberry_Pi/gspreads_package/gspreads_functions.py
@@ -4,7 +4,8 @@ def gsAirData (timeStamp, equipament, temperature, humidity, pressure, gasResist
 
     scope = ['https://spreadsheets.google.com/feeds']
 
-    credentials = ServiceAccountCredentials.from_json_keyfile_name('perfect-transit-298617-ae8840d45b6e.json', scope)
+    credentials = ServiceAccountCredentials.from_json_keyfile_name('/Public/Dev/IoT2B/MS430/Raspberry_Pi/gspreads_package/ls
+    perfect-transit-298617-ae8840d45b6e.json', scope)
 
     gc = gspread.authorize(credentials)
 
