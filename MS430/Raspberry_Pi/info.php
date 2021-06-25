49e96516e629f3dcb7fa17a12e5049e5f97d2129
leandrodmoreira <leandrodmoreira@gmail.com>
2021-06-25 17:06:13 -0300

    Select PHP

--- MS430/Raspberry_Pi/info.php
+++ MS430/Raspberry_Pi/info.php
@@ -1,7 +1,31 @@
-<?php
+<html>
+    <head>
+        <title>Iot 2 Home</title>
+    </head>
+    <body>
+        <?php 
+            echo "<p>Lendo dados de Velocidade da Internet</p>";
+            echo "ID - Data / Hora - Equipamento - Download - Upload";
+            echo "<br>";
 
-echo "hello world";
-echo date('Y-m-d H:i:s');
-phpinfo();
+            $user = "root"; 
+            $password = "Wall@1979"; 
+            $database = "homeiot"; 
 
-?>
+            # O hostname deve ser sempre localhost 
+            $hostname = "locahost";
+ 
+            # Conexão MySQL com PHP 7
+            $conexao = mysqli_connect('192.168.0.38','root','@cthum@1979');
+            $banco = mysqli_select_db($conexao,'iot2b');
+            mysqli_set_charset($conexao,'utf8');
+ 
+            $sql = mysqli_query($conexao,"select * from airData order by id desc") or die("Erro");
+            
+            while($dados=mysqli_fetch_assoc($sql))
+                {   
+                    echo $dados['id']." - ".$dados['temperature']." - ".$dados['humidity']." - ".$dados['pressure']." - ".$dados['gasResistence'].'<br>';
+                }
+        ?>
+    </body>
+</html>
