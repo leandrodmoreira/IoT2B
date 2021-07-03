27fe23e3e5c14f00cc26ae1fc7be9cdfd754fdaf
leandrodmoreira <leandrodmoreira@gmail.com>
2021-07-03 03:24:42 -0300

    Test new php mysql connection

---
+++ MS430/Raspberry_Pi/teste.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <title>Iot 2 Home</title>
+    </head>
+    <body>
+        <?php 
+            echo "<p>Lendo dados de Velocidade da Internet</p>";
+            echo "ID - Data / Hora - Equipamento - Download - Upload";
+            echo "<br>";
+ 
+            # Conexão MySQL com PHP 7
+            $conexao = mysqli_connect('192.168.0.38','root','@cthum@1979','iot2b');
+            mysqli_select_db($conexao,iot2b);
+            mysqli_set_charset($conexao,'utf8');
+ 
+            $sql = mysqli_query($conexao,"select * from airData order by id desc") or die("Erro");
+            
+            while($dados=mysqli_fetch_assoc($sql))
+                {   
+                    echo $dados['id'].'<br>';
+                }
+            mysqli_close($conexao);
+        ?>
+    </body>
+</html>
