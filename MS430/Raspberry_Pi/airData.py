b6ffede11fd3207a4834128efc4e2ad10dd091c3
leandrodmoreira <leandrodmoreira@gmail.com>
2021-11-11 22:28:50 -0300

    Google Sheets Data Record

--- MS430/Raspberry_Pi/airData.py
+++ MS430/Raspberry_Pi/airData.py
@@ -88,12 +88,12 @@ finally:
 
 from oauth2client.service_account import ServiceAccountCredentials
 scope = ['https://spreadsheets.google.com/feeds']
-credentials = ServiceAccountCredentials.from_json_keyfile_name('iot2bv2-ebd620022c11.json', scope)
+credentials = ServiceAccountCredentials.from_json_keyfile_name('/home/pi/Public/Dev/IoT2B/MS430/Raspberry_Pi/iot2bv2-ebd620022c11.json', scope)
 gc = gspread.authorize(credentials)
 wks = gc.open_by_key('1Sjq3HmkMCt6LhME6F9rhteMYT1DlhseJwpMEFZc5qU4')
 worksheet = wks.get_worksheet(0)
 
-arqCont = open("cont.txt","r")
+arqCont = open("/home/pi/Public/dev/IoT2B/MS430/Raspberry_Pi/cont.txt","r")
 linha = arqCont.read(100)
 
 contA = 'A' + str(linha)
